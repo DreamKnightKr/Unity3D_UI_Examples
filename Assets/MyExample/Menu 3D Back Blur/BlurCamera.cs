23898cfdbd4a5e53dc6754ebad7a3e70ccdc9997
DreamKnightKr <dreamknightkr@gmail.com>
2015-09-15 06:44:48 +0900

    * 내 나름의 블러(Blur) 효과 써보기.

---
+++ Assets/MyExample/Menu 3D Back Blur/BlurCamera.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class BlurCamera : MonoBehaviour {
+	public RawImage targetImg;
+
+	// Use this for initialization
+	void Start () {
+		RenderTexture rendT = new RenderTexture(Screen.width/3, Screen.height/3, 16, RenderTextureFormat.ARGB32);
+		targetImg.texture = rendT;
+
+		Camera cam = GetComponent<Camera> ();
+		cam.targetTexture = rendT;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
